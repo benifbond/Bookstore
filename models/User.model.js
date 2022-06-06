4763176b1c56568afe3ffd91e41767c208b3ed5c
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-06-06 11:38:45 +0200

    create routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+// TODO: Please make sure you edit the user model to whatever makes sense in this cas
 const userSchema = new Schema(
   {
     username: {
