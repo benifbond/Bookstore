6e0bf275914941a3183dd1bcfbbc2b17cf1d0919
Hanin Mez <haninjamalmzher@gmail.com>
2022-06-07 16:55:06 +0200

    add models

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,23 @@
 const router = require("express").Router();
-
+const Book = require('../models/Book.model')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get('/books', async (req, res, next) => {
+  let allBooks = await Book.find()
+  console.log("ALL BOOOOOOOkS", allBooks);
+  res.render('books', { allBooks })
+});
 
+router.get('/books/:id', async (req, res, next) => {
+  let allBooks = await Book.find()
+  let data = res.send(req.params['id'])
+  let book = allBooks[data]
+  res.render('oneBook', { book })
+});
 
 
 
