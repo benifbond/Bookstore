5ff7b1393d58a2d8f7290c1a1e86d274d5922078
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-06-08 21:30:22 +0200

    userProfile style

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,47 +1,41 @@
-const { Router } = require('express')
+const { Router } = require("express")
 const router = new Router()
 
 
-const mongoose = require('mongoose') // <== has to be added
-const bcryptjs = require('bcryptjs')
+const mongoose = require("mongoose") // <== has to be added
+const bcryptjs = require("bcryptjs")
 const saltRounds = 10
+const Book = require("../models/Book.model")
+const User = require("../models/User.model")
 
-const Book = require('../models/Book.model')
-const User = require('../models/User.model')
-const Book = require('../models/Book.model')
-const { findById } = require('../models/User.model')
-
-router.get('/books', async (req, res, next) => {
+const { findById } = require("../models/User.model")
+router.get("/books", async (req, res, next) => {
   let allBooks = await Book.find()
-  console.log("ALL BOOOOOOOkS",allBooks);
-  res.render('movies', { allMovies })
+  
+  res.render("movies", { allMovies })
 });
-
-router.get('/books/:id', async (req, res, next) => {
+router.get("/books/:id", async (req, res, next) => {
   const {id} = req.params.id;
   let book = await Book.findById(id)
-  res.render('/oneBook', { book })
+  res.render("/oneBook", { book })
 });
 // GET route ==> to display the signup form to users
-router.get('/signup', (req, res) => res.render('auth/signup'))
-
-
-router.post('/signup', (req, res, next) => {
+router.get("/signup", (req, res) => res.render("auth/signup"))
+router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body
-
   // make sure users fill all mandatory fields:
   if (!username || !email || !password) {
-    res.render('auth/signup', {
-      errorMessage: 'All fields are mandatory. Please provide your username, email and password.',
+    res.render("auth/signup", {
+      errorMessage: "All fields are mandatory. Please provide your username, email and password.",
     })
     return
   }
   // make sure passwords are strong:
   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
   if (!regex.test(password)) {
-    res.status(500).render('auth/signup', {
+    res.status(500).render("auth/signup", {
       errorMessage:
-        'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.',
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
     })
     return
   }
@@ -57,15 +51,15 @@ router.post('/signup', (req, res, next) => {
     })
     .then(userFromDB => {
       // console.log("Newly created user is: ", userFromDB);
-      res.redirect('/login')
+      res.redirect("/login")
     })
     .catch(error => {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render('auth/signup', { errorMessage: error.message })
+        res.status(500).render("auth/signup", { errorMessage: error.message })
       } else if (error.code === 11000) {
-        res.status(500).render('auth/signup', {
+        res.status(500).render("auth/signup", {
           errorMessage:
-            'Username and email need to be unique. Either username or email is already used.',
+            "Username and email need to be unique. Either username or email is already used.",
         })
       } else {
         next(error)
@@ -73,35 +67,31 @@ router.post('/signup', (req, res, next) => {
     }) // close .catch()
 })
 //////////// L O G I N ///////////
-router.get('/login', (req, res) => res.render('auth/login'))
-
-router.post('/login', async(req, res, next) => {
+router.get("/login", (req, res) => res.render("auth/login"))
+router.post("/login", async(req, res, next) => {
   const { email, password } = req.body
-  if (email === '' || password === '') {
-    res.render('auth/login', {
-      errorMessage: 'Please enter both, email and password to login.',
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login.",
     })
     return
   }
-
-  const user = await User.findOne({ email }) 
+  const user = await User.findOne({ email })
       if (!user?.email) {
-        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' })
+        res.render("auth/login", { errorMessage: "Email is not registered. Try with other email." })
         return
       }
       else if (bcryptjs.compareSync(password, user.passwordHash)) {
         //******* SAVE THE USER IN THE SESSION ********//
         req.session.currentUser = user
-  res.redirect('/userProfile')
+  res.redirect("/userProfile")
 } else {
-        // if the two passwords DON'T match, render the login form again
+        // if the two passwords DON"T match, render the login form again
         // and send the error message to the user
-        res.render('auth/login', { errorMessage: 'Incorrect password.' })
+        res.render("auth/login", { errorMessage: "Incorrect password." })
       }
-      
     })
-
-router.get('/userProfile', async (req, res) => {
+router.get("/userProfile", async (req, res) => {
   const currentUser = req.session.currentUser;
   let allBooks = await Book.find()
   const userBooks = {
@@ -109,22 +99,16 @@ router.get('/userProfile', async (req, res) => {
     allBooks
   }
   console.log(userBooks)
-  res.render('users/user-profile', { userBooks })
-  
+  res.render("users/user-profile", { userBooks })
 })
-
-router.get('/book/:id', async (req, res, next) => {
-  
+router.get("/book/:id", async (req, res, next) => {
   const bookId = req.params.id
   let oneBook = await Book.findById(bookId)
   console.log(oneBook)
- 
-  res.render('oneBook', { oneBook })
+  res.render("oneBook", { oneBook })
 });
-
-
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   req.session.destroy()
-  res.redirect('/')
+  res.redirect("/")
 })
-module.exports = router
+module.exports = router 
